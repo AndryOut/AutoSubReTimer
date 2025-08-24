b4b0a7a3c112d987387de93ccb63168adb08efc4
AndryOut <gaglianone8@gmail.com>
2025-08-24 17:34:56 +0200

    Demucs Improvements
    - Added two parameters to maximize quality and reduce artifacts in voice separations.
    
    - Added a parameter to speed up the process.
    In CPU mode, there will be a significant speed increase.
    In GPU mode, only processes running on the CPU will be accelerated, so the performance gain will be limited.
    
    (This means that with fewer artifacts and noise, the timing adjustment precision may be higher in some situations)

--- Scripts/Whisper Miglioramento Timing/Fase0.py
+++ Scripts/Whisper Miglioramento Timing/Fase0.py
@@ -40,11 +40,11 @@ else:
 
 # Comando per eseguire Demucs con device dinamico
 os.environ['PATH'] += os.pathsep + os.path.join(project_path, "ffmpeg", "bin")
-demucs_command = f'"{python_executable}" -m demucs --two-stems=vocals -d {device} -o "{output_dir}" "{input_path}"'
+demucs_command = f'"{python_executable}" -m demucs --two-stems=vocals --clip-mode clamp --float32 --jobs 2 -d {device} -o "{output_dir}" "{input_path}"'
 
 try:
     print(f"Esecuzione di Demucs: {demucs_command}")
-    subprocess.run(demucs_command, shell=True, check=True)
+    subprocess.run(demucs_command, check=True)
 
     # Percorso della cartella generata automaticamente da Demucs
     demucs_output_dir = os.path.join(output_dir, "htdemucs", os.path.splitext(os.path.basename(input_path))[0])
@@ -55,12 +55,6 @@ try:
     if os.path.exists(vocals_file):
         shutil.move(vocals_file, renamed_vocals_file)
         print(f"`vocals.wav` rinominato in: {renamed_vocals_file}")
-
-    # Rimuovi il file "no_vocals.wav"
-    no_vocals_file = os.path.join(demucs_output_dir, "no_vocals.wav")
-    if os.path.exists(no_vocals_file):
-        os.remove(no_vocals_file)
-        print(f"`no_vocals.wav` eliminato.")
 
     # Rimuovi la cartella `htdemucs`
     shutil.rmtree(os.path.join(output_dir, "htdemucs"))
